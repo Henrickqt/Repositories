34130a20c9f2caf54fbecd2d2bcf2fa0c16467bb
Henrickqt <pedro.msg@hotmail.com>
2020-11-03 04:27:31 -0200

    Reverse Engineering

---
+++ WebAPI/Contexts/RepositoriesContext.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Repositories.WebAPI.Models;
+
+namespace Repositories.WebAPI.Contexts
+{
+    public partial class RepositoriesContext : DbContext
+    {
+        public RepositoriesContext()
+        {
+        }
+
+        public RepositoriesContext(DbContextOptions<RepositoriesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Developer> Developer { get; set; }
+        public virtual DbSet<Project> Project { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. 
+                // See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=ADM-PC\\SQLEXPRESS;Initial Catalog=Repositories;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Developer>(entity =>
+            {
+                entity.HasKey(e => e.Email);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(64)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Bio)
+                    .IsRequired()
+                    .HasMaxLength(256);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(128);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(32)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Project>(entity =>
+            {
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(256);
+
+                entity.Property(e => e.IdDeveloper)
+                    .IsRequired()
+                    .HasMaxLength(64)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Languages)
+                    .IsRequired()
+                    .HasMaxLength(64);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(128);
+
+                entity.HasOne(d => d.IdDeveloperNavigation)
+                    .WithMany(p => p.Project)
+                    .HasForeignKey(d => d.IdDeveloper)
+                    .HasConstraintName("FK_Project_Developer");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
